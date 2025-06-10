65efe2413f9bd0c8f9c119324018036676b8d7cd
André Tutas <tutankhamal@outlook.com>
2025-06-10 02:33:06 -0300

    new colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,40 +30,7 @@ backToTopButton.addEventListener("click", () => {
   window.addEventListener("scroll", handleScroll)
   hamburger.addEventListener("click", toggleMenu)
 
-  // Initialize FAQ accordions
-  faqItems.forEach((item) => {
-    const question = item.querySelector(".faq-question")
-    question.addEventListener("click", () => {
-      item.classList.toggle("active")
-    })
-  })
-
-
-
-
-  
-  // Contact Form Submission
-  const contactForm = document.getElementById("contactForm")
-  if (contactForm) {
-    contactForm.addEventListener("submit", function (e) {
-      e.preventDefault()
-
-      // Simulate form submission
-      const submitButton = this.querySelector('button[type="submit"]')
-      const originalText = submitButton.textContent
-
-      submitButton.disabled = true
-      submitButton.textContent = "Enviando..."
-
-      // Simulate API call
-      setTimeout(() => {
-        alert("Mensagem enviada com sucesso! Entraremos em contato em breve.")
-        contactForm.reset()
-        submitButton.disabled = false
-        submitButton.textContent = originalText
-      }, 1500)
-    })
-  }
+   
 
   // Functions
   function handleScroll() {
@@ -81,232 +48,12 @@ backToTopButton.addEventListener("click", () => {
   
     console.log("Scroll:", scrollTop, "Último Scroll:", lastScrollTop); // Para depuração
   }
-  
-
-  function toggleMenu() {
-    hamburger.classList.toggle("active")
-    navLinks.classList.toggle("active")
-    document.body.classList.toggle("no-scroll")
-  }
-
-  
-  // Hero Logo Animation
-  const heroLogo = document.querySelector(".hero-logo")
-  if (heroLogo) {
-    heroLogo.addEventListener("mouseover", () => {
-      heroLogo.style.filter = "drop-shadow(0 0 15px var(--primary-color))"
-    })
-
-    heroLogo.addEventListener("mouseout", () => {
-      heroLogo.style.filter = ""
-    })
-  }
-
-  const heroMainContent = document.getElementById("heroMainContent");
-  const heroAdContent = document.getElementById("heroAdContent");
-  const heroSlider = document.getElementById("heroSlider");
-  
-  // Efeito de entrada
-  window.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-      heroMainContent.classList.add("mosaic-in");
-    }, 100);
-  });
-  
-  let showingMain = true;
-  function toggleHeroContent() {
-    if (heroMainContent && heroAdContent && heroSlider) {
-      heroSlider.classList.add("slide-left");
-      setTimeout(() => {
-        if (showingMain) {
-          heroMainContent.style.display = "none";
-          heroAdContent.style.display = "flex";
-          heroAdContent.classList.add("mosaic-in");
-          heroMainContent.classList.remove("mosaic-in");
-        } else {
-          heroAdContent.style.display = "none";
-          heroMainContent.style.display = "flex";
-          heroMainContent.classList.add("mosaic-in");
-          heroAdContent.classList.remove("mosaic-in");
-        }
-        heroSlider.classList.remove("slide-left");
-        showingMain = !showingMain;
-      }, 900); // tempo igual ao transition da .hero-slider
-    }
-  }
-  
-  setInterval(toggleHeroContent, 10000);
-
-  
-
-  // Video Card Hover Effects
-  const videoCards = document.querySelectorAll(".video-card")
-  videoCards.forEach((card) => {
-    card.addEventListener("mouseover", () => {
-      const playButton = card.querySelector(".play-button")
-      if (playButton) {
-        playButton.style.opacity = "1"
-      }
-    })
-
-    card.addEventListener("mouseout", () => {
-      const playButton = card.querySelector(".play-button")
-      if (playButton) {
-        playButton.style.opacity = "0"
-      }
-    })
-  })
+    
 })
 
 
-  // Partenrs Carroussel close to Footer
-document.addEventListener("DOMContentLoaded", () => {
-  const track = document.querySelector(".carousel-track");
-  const container = document.querySelector(".carousel-container");
-  let slides = Array.from(track.children);
-  const pauseTime = 2000;
-  const slideTime = 3000;
-  let paused = false;
-  let timeout;
+// Background Universe //
 
-  function animateBrightness(img, from, to, duration = 1500) {
-    const startTime = performance.now();
-
-    function step(currentTime) {
-      const elapsed = currentTime - startTime;
-      const progress = Math.min(elapsed / duration, 1);
-      const brightness = from + (to - from) * progress;
-      img.style.filter = `brightness(${brightness.toFixed(2)})`;
-
-      if (progress < 1) {
-        requestAnimationFrame(step);
-      }
-    }
-
-    requestAnimationFrame(step);
-  }
-
-  function activateZoom() {
-    slides.forEach(s => {
-      const img = s.querySelector("img");
-      s.classList.remove("active");
-      img.style.transform = "scale(1)";
-      img.style.filter = "brightness(1)";
-    });
-
-    const activeSlide = slides[0];
-    const activeImg = activeSlide.querySelector("img");
-    activeSlide.classList.add("active");
-    activeImg.style.transform = "scale(1.45)";
-    animateBrightness(activeImg, 1, 1.5, 1500);
-  }
-
-  function next() {
-    if (paused) return;
-    track.style.transition = `transform ${slideTime}ms cubic-bezier(0.77, 0, 0.175, 1)`;
-    track.style.transform = `translateX(-100%)`;
-    track.addEventListener("transitionend", onTransitionEnd, { once: true });
-  }
-
-  function onTransitionEnd() {
-    track.style.transition = 'none';
-    track.style.transform = 'translateX(0)';
-    const first = slides.shift();
-    track.appendChild(first);
-    slides.push(first);
-    activateZoom();
-    timeout = setTimeout(next, pauseTime);
-  }
-
-  slides.forEach(slide => {
-    slide.querySelector("img").addEventListener("click", e => {
-      const link = e.currentTarget.dataset.link;
-      if (link) window.open(link, "_blank");
-    });
-  });
-
-  container.addEventListener("mouseenter", () => {
-    paused = true;
-    clearTimeout(timeout);
-    track.style.transition = '';
-  });
-
-  container.addEventListener("mouseleave", () => {
-    paused = false;
-    setTimeout(next, 300);
-  });
-
-  activateZoom();
-  timeout = setTimeout(next, pauseTime);
-});
-
-
-
-
-
-
-
-
-// new partners live tab 
-document.addEventListener("DOMContentLoaded", () => {
-  const shineTrack = document.querySelector(".shine-carousel-track");
-  const shineContainer = document.querySelector(".shine-carousel-container");
-  let shineSlides = Array.from(shineTrack.children);
-  const pauseTime = 2800;
-  const slideTime = 2000;
-  let shinePaused = false;
-  let shineTimeout;
-
-  function activateShine() {
-    shineSlides.forEach(s => s.classList.remove("active"));
-    shineSlides[0].classList.add("active");
-  }
-
-  function nextShine() {
-    if (shinePaused) return;
-    shineTrack.style.transition = `transform ${slideTime}ms ease-in-out`;
-    shineTrack.style.transform = `translateX(-100%)`;
-    shineTrack.addEventListener("transitionend", onShineTransitionEnd, { once: true });
-  }
-
-  function onShineTransitionEnd() {
-    shineTrack.style.transition = 'none';
-    shineTrack.style.transform = 'translateX(0)';
-    const first = shineSlides.shift();
-    shineTrack.appendChild(first);
-    shineSlides.push(first);
-    activateShine();
-    shineTimeout = setTimeout(nextShine, pauseTime);
-  }
-
-  shineSlides.forEach(slide => {
-    slide.querySelector("img").addEventListener("click", e => {
-      const link = e.currentTarget.dataset.link;
-      if (link) window.open(link, "_blank");
-    });
-  });
-
-  shineContainer.addEventListener("mouseenter", () => {
-    shinePaused = true;
-    clearTimeout(shineTimeout);
-    shineTrack.style.transition = '';
-  });
-
-  shineContainer.addEventListener("mouseleave", () => {
-    shinePaused = false;
-    setTimeout(nextShine, 300);
-  });
-
-  activateShine();
-  shineTimeout = setTimeout(nextShine, pauseTime);
-});
-
-
-
-
-
-
-    
 const canvas = document.getElementById('universe');
 const ctx = canvas.getContext('2d');
 let w = canvas.width = window.innerWidth;
@@ -1465,18 +1212,7 @@ animate();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+// Hero Terminal Style //
 
 const codeLines = [
   "const terminalEl = document.getElementById('terminal-content')",
@@ -1521,9 +1257,10 @@ function showHeroContent() {
 }
 
 const phrases = [
-  "Web Designer / Front-End Developer",
-  "Dê vida a sua criatividade.",
-  "Eleve seu projeto ao proximo nível."
+  "Web Designer & Front-End Dev",
+  "Dê vida a sua imaginação. 💜",
+  "Mostre sua Identidade Visual. 📈",
+  "Expanda o alcance da sua marca. 🌎"
 ];
 
 let currentPhrase = 0;
@@ -1537,10 +1274,10 @@ function startTypingCycle() {
 
   if (!isDeleting && charIndex < current.length) {
     charIndex++;
-    setTimeout(startTypingCycle, 60);
+    setTimeout(startTypingCycle, 80);
   } else if (isDeleting && charIndex > 0) {
     charIndex--;
-    setTimeout(startTypingCycle, 30);
+    setTimeout(startTypingCycle, 50);
   } else {
     if (!isDeleting) {
       isDeleting = true;
@@ -1558,185 +1295,3 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Section Viewport effect
-document.addEventListener("DOMContentLoaded", () => {
-  const sections = document.querySelectorAll(".animated-section");
-
-  const effects = [
-    "effect-car-left",
-    //"effect-car-right",
-    "effect-car-top"
-    // "effect-car-bottom" removido
-  ];
-
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const section = entry.target;
-        const content = section.querySelector(".animated-content");
-
-        if (content && !content.classList.contains("animated")) {
-          const randomEffect = effects[Math.floor(Math.random() * effects.length)];
-          section.classList.add(randomEffect);
-          content.classList.add("animated");
-        }
-
-        observer.unobserve(section); // remove se não quiser repetir
-      }
-    });
-  }, {
-    threshold: 0.4
-  });
-
-  sections.forEach(section => observer.observe(section));
-});
-
-
-
-const animatedContent2 = document.querySelectorAll('.animated-content2');
-
-const observer2 = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-            observer2.unobserve(entry.target); // Opcional: para animar apenas uma vez
-        }
-    });
-}, {
-    threshold: 0.1 // Ajuste conforme a necessidade de visibilidade para disparar a animação
-});
-
-animatedContent2.forEach(element => {
-    observer2.observe(element);
-});
-
-
-
-// Partner Channers Carroussel
-
-const partnerData = [
-  { name: "Defenestrando Jogos", url: "https://www.youtube.com/@defenestrandojogos", img: "/assets/images/partnerch/defenestrando.webp" },
-  { name: "Canal Maddrugamers", url: "https://www.youtube.com/@maddrugamers", img: "/assets/images/partnerch/maddrugamers.webp" },
-  { name: "Direto dos Consoles", url: "https://www.youtube.com/@DiretodosConsoles", img: "/assets/images/partnerch/diretodosconsoles.webp" },
-  { name: "Retrozera", url: "https://www.youtube.com/@Retrozera", img: "/assets/images/partnerch/retrozera.webp" },
-  { name: "Detona Bits", url: "https://www.youtube.com/@Detonabits", img: "/assets/images/partnerch/detonabits.webp" },
-  { name: "Alan Joga+", url: "https://www.youtube.com/@alan.jogamais", img: "/assets/images/partnerch/jogamais.webp" },
-  { name: "Gameplay Br", url: "https://www.youtube.com/@gameplaybr1746", img: "/assets/images/partnerch/gameplaybr.webp" },
-  { name: "GuaxaZX", url: "https://www.youtube.com/@GuaxaZX", img: "/assets/images/partnerch/guaxazx.webp" },
-  { name: "Nostal GAMES Retro", url: "https://www.youtube.com/@nostalgamesretro", img: "/assets/images/partnerch/nostalgamesretro.webp" },
-  { name: "Jana Gamer", url: "https://www.youtube.com/@JanaGamer", img: "/assets/images/partnerch/janagamer.webp" },
-  { name: "Anielleom", url: "https://www.youtube.com/@anielleom", img: "/assets/images/partnerch/anielleom.webp" },
-  { name: "Mina dos Games (Amanda Nicoly)", url: "https://www.youtube.com/@minadosgamesamandanicoly6493", img: "/assets/images/partnerch/minadosgames.webp" },
-  { name: "Attomic Games TV", url: "https://www.youtube.com/@Attomicgames", img: "/assets/images/partnerch/attomicgames.webp" },
-  { name: "OpassadoRecentedosGames", url: "https://www.youtube.com/@OpassadoRecenteDosGames", img: "/assets/images/partnerch/opassadorecente.webp" },
-  { name: "Gorilas Sisters", url: "https://www.youtube.com/@GorilasSisters", img: "/assets/images/partnerch/gorilassisters.webp" },
-  { name: "Games das antigas", url: "https://www.youtube.com/@Gamesdasantigas", img: "/assets/images/partnerch/gamesdasantigas.webp" },
-  { name: "MiRiNiOR GAMES", url: "https://www.youtube.com/@MiRiNiOR", img: "/assets/images/partnerch/miriniorgames.webp" },
-  { name: "Fernando Lucas", url: "https://www.youtube.com/@fernando_lcs", img: "/assets/images/partnerch/fernandolucas.webp" },
-  { name: "Toloi Games", url: "https://www.youtube.com/@ToloiGames", img: "/assets/images/partnerch/toloigames.webp" },
-  { name: "Creative Games", url: "https://www.youtube.com/@CreativeGamesOficial", img: "/assets/images/partnerch/creativegames.webp" }
-];
-
-const container = document.querySelector(".channel-carousel-container");
-const track = document.querySelector(".channel-carousel-track");
-
-function shuffle(array) {
-  return array.sort(() => Math.random() - 0.5);
-}
-
-function createSlides() {
-  const shuffled = shuffle([...partnerData]);
-  const slides = shuffled.map(data => {
-    const div = document.createElement("div");
-    div.className = "channel-slide";
-    div.innerHTML = `
-      <a href="${data.url}" target="_blank">
-        <img src="${data.img}" alt="${data.name}">
-        <p>${data.name}</p>
-      </a>
-    `;
-    return div;
-  });
-
-  slides.forEach(slide => track.appendChild(slide));
-  slides.forEach(slide => track.appendChild(slide.cloneNode(true))); // Duplicar para loop
-}
-
-createSlides();
-
-let scrollSpeed = 0.5;
-let scrollDirection = 1;
-
-function animateScroll() {
-  container.scrollLeft += scrollSpeed * scrollDirection;
-
-  if (container.scrollLeft >= track.scrollWidth / 2) {
-    container.scrollLeft = 0;
-  } else if (container.scrollLeft <= 0) {
-    container.scrollLeft = track.scrollWidth / 2;
-  }
-
-  requestAnimationFrame(animateScroll);
-}
-
-animateScroll();
-
-function updateScrollSpeed(x) {
-  const width = container.offsetWidth;
-  const left = width * 0.2;
-  const right = width * 0.8;
-
-  if (x < left) {
-    scrollDirection = -1;
-    scrollSpeed = 1.5;
-  } else if (x > right) {
-    scrollDirection = 1;
-    scrollSpeed = 1.5;
-  } else {
-    scrollDirection = 1;
-    scrollSpeed = 0.5;
-  }
-}
-
-container.addEventListener("mousemove", e => updateScrollSpeed(e.clientX));
-container.addEventListener("touchmove", e => {
-  if (e.touches.length > 0) updateScrollSpeed(e.touches[0].clientX);
-});
-
-
-
-window.addEventListener('scroll', () => {
-  const canvas = document.querySelector('canvas');
-  if (canvas) {
-    canvas.style.top = window.scrollY + 'px';
-  }
-});
